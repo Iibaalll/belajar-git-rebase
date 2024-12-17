7226585d3a8c1adb02cc579b7b2c8de385d367e4
Iibaalll <iqbal1901.mi@gmail.com>
2024-12-18 03:40:03 +0700

    menambah fitur ganjil genap dengan js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("Hello World");
+let ulang = true;
+while (ulang) {
+  let angka = prompt("Masukan Angka");
+  if (angka % 2 == 0) {
+    alert("Genap");
+  } else if (angka % 2 == 1) {
+    alert("ganjil");
+  } else {
+    alert("Yang anda masukan bukan angka");
+  }
+  ulang = confirm("Ulang?");
+}
